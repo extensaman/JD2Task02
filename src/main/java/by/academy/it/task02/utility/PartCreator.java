4ffea4bae1e85863bb85ebf6ee6636cefe0f4a3c
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-03-11 22:42:02 +0300

    Some change in PartCreator

--- src/main/java/by/academy/it/task02/utility/PartCreator.java
+++ src/main/java/by/academy/it/task02/utility/PartCreator.java
@@ -8,34 +8,16 @@ public class PartCreator {
 
     public static Part create(PartTypes type) {
         switch (type) {
-            case BODY -> {
-                return new Body();
-            }
-            case CPU -> {
-                return new CPU();
-            }
-            case HDD -> {
-                return new HDD();
-            }
-            case HEAD -> {
-                return new Head();
-            }
-            case LEFT_HAND -> {
-                return new LeftHand();
-            }
-            case LEFT_LEG -> {
-                return new LeftLeg();
-            }
-            case RAM -> {
-                return new RAM();
-            }
-            case RIGHT_HAND -> {
-                return new RightHand();
-            }
-            case RIGHT_LEG -> {
-                return new RightLeg();
-            }
+            case BODY: return new Body();
+            case CPU: return new CPU();
+            case HDD: return new HDD();
+            case HEAD: return new Head();
+            case LEFT_HAND: return new LeftHand();
+            case LEFT_LEG: return new LeftLeg();
+            case RAM: return new RAM();
+            case RIGHT_HAND: return new RightHand();
+            case RIGHT_LEG: return new RightLeg();
+            default: return null;
         }
-        return null;
     }
 }
