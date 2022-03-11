c26038191844a050cc399dfdf5e92d7a3f152600
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2022-03-11 22:15:58 +0300

    Changed PartCreator

--- src/main/java/by/academy/it/task02/utility/PartCreator.java
+++ src/main/java/by/academy/it/task02/utility/PartCreator.java
@@ -7,40 +7,34 @@ import by.academy.it.task02.entity.part.enums.PartTypes;
 public class PartCreator {
 
     public static Part create(PartTypes type) {
-        try {
-            do {
-                switch (type) {
-                    case BODY -> {
-                        return new Body();
-                    }
-                    case CPU -> {
-                        return new CPU();
-                    }
-                    case HDD -> {
-                        return new HDD();
-                    }
-                    case HEAD -> {
-                        return new Head();
-                    }
-                    case LEFT_HAND -> {
-                        return new LeftHand();
-                    }
-                    case LEFT_LEG -> {
-                        return new LeftLeg();
-                    }
-                    case RAM -> {
-                        return new RAM();
-                    }
-                    case RIGHT_HAND -> {
-                        return new RightHand();
-                    }
-                    case RIGHT_LEG -> {
-                        return new RightLeg();
-                    }
-                }
-            } while (true);
-        } catch (Exception e) {
-            e.printStackTrace();
+        switch (type) {
+            case BODY -> {
+                return new Body();
+            }
+            case CPU -> {
+                return new CPU();
+            }
+            case HDD -> {
+                return new HDD();
+            }
+            case HEAD -> {
+                return new Head();
+            }
+            case LEFT_HAND -> {
+                return new LeftHand();
+            }
+            case LEFT_LEG -> {
+                return new LeftLeg();
+            }
+            case RAM -> {
+                return new RAM();
+            }
+            case RIGHT_HAND -> {
+                return new RightHand();
+            }
+            case RIGHT_LEG -> {
+                return new RightLeg();
+            }
         }
         return null;
     }
